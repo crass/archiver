d177749cbb08f9dbfc2a6c8277a4bfed2468a6af
crass <crass@berlios.de>
2013-09-27 02:15:08 -0500

    Add version as specified in install.rdf to name of build file.

--- config_build.sh
+++ config_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Build config for build.sh
-APP_NAME=shelve
+VERSION=$(grep "em:version" "install.rdf"|sed -e 's|<[^>]\+>||g' -e 's|\s\+||g')
+APP_NAME="shelve-$VERSION"
 CHROME_PROVIDERS="content locale skin"
 CLEAN_UP=1
 ROOT_FILES=
